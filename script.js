6a14b53ed72f141a279ee2cb33908c8f03822ca7
Lasse Vestergaard <lassevestergaard95@hotmail.com>
2023-09-12 12:24:35 +0200

    done funktionner delvis done

--- script.js
+++ script.js
@@ -1,46 +1,77 @@
 "use strict";
-// ved at gøre consten global, kan vi bruge den flere steder i vores JS
-const activeArr = [];
 
+// globale arrays
+const activeArr = [];
+const doneArr = [];
+
+// vi har defineret denne funktion med onclick i vores HTML. Funktionen tager de værdier den får fra task og quantity, laver dem om til objekter og bliver derefter smidt i et array med navnet activeArr.
 function storeInput() {
   const task = document.getElementById("task");
   const quantity = document.getElementById("quantity");
 
-  const taskVærdi = task.value;
-  const quantityVærdi = quantity.value;
+  const taskValue = task.value;
+  const quantityValue = quantity.value;
+// done bliver som udgangspunktet defineret til 'false' da den jo er aktiv indtil den bliver checked.
+  const taskObj = { task: taskValue, quantity: quantityValue, done: false };
 
-  const taskObj = { task: taskVærdi, quantity: quantityVærdi };
-
-//   her tilføjer du det enkelte objekt til det globale array (activeArr)
+  // her bliver vores nye objekt pushet ind i vores array med aktive tasks.
   activeArr.push(taskObj);
 
   addTask();
 
-//   smart funktion som gør at hver gang du trykker på "tilføj", så resetter den de to inputfelter så du ikke skal slette det du skrev lige før.
-  task.value ="";
+  // de to inputfelter bliver resettet så man som bruger ikke skal slette det man skrev tidligere.
+  task.value = "";
   quantity.value = "";
 
   console.log(activeArr);
 }
+
+// denne funktion tager activeArr og appender det nye objekts værdier til list items. 
 function addTask() {
-  const ul = document.getElementById("active_list")
+  const ulActive = document.getElementById("active_list");
+  ulActive.innerHTML = "";
 
-
-
-
-//   denne funktion giver listen en "reset" så den kun viser det opdaterede array. Hvis den ikke er det vil den tilføje samtlige objekter hver gang du trykker "tilføj"
-  ul.innerHTML = "";
-
+  // vi looper igennem hvert objekt. Udover at vise hvert objekt, vil vi gerne have en checkbox som vi kan hakke af i når opgaven er done. Dette fører os ned i en ny funktion.
   activeArr.forEach((taskObj) => {
     const li = document.createElement("li");
     const checkbox = document.createElement("input");
+
     checkbox.type = "checkbox";
     checkbox.checked = taskObj.done;
-    
-    
+    checkbox.addEventListener("change", () => {
+      taskObj.done = checkbox.checked;
+      if (taskObj.done) {
+        moveToDone(taskObj);
+      }
+    });
+
     const taskText = document.createTextNode(`${taskObj.task} ${taskObj.quantity}`);
-    li.appendChild(checkbox)
-    li.appendChild(taskText)
-    ul.appendChild(li);
+    li.appendChild(checkbox);
+    li.appendChild(taskText);
+    ulActive.appendChild(li);
   });
+}
+
+function moveToDone(taskObj) {
+  const ulDone = document.getElementById("done_list");
+  
+  if (!taskObj.hasOwnProperty("done")) {
+    taskObj.done = false; // Initialize "done" property if it doesn't exist
+  }
+
+  doneArr.push(taskObj);
+  const index = activeArr.indexOf(taskObj);
+  if (index !== -1) {
+    activeArr.splice(index, 1);
+  }
+  
+  ulDone.innerHTML = ""; // Clear the done list and recreate it.
+  doneArr.forEach((taskObj) => {
+    const li = document.createElement("li");
+    const taskText = document.createTextNode(`${taskObj.task} ${taskObj.quantity}`);
+    li.appendChild(taskText)
+    ulDone.appendChild(li);
+  });
+  
+  addTask();
 }
